cee543eab2dcaaa159f2a84d902bdfa068b839b5
suuuth <suther98@msu.edu>
2018-11-06 13:29:31 -0500

    initail commit

---
+++ script-db.js
@@ -0,0 +1,64 @@
+
+var config = {
+  apiKey: 'AIzaSyC5U0W-RKT-JnPfTHhGbxKvpPojEOt-7WY',
+  authDomain: 'vf-test-c0a02.firebaseapp.com',
+  databaseURL: 'https://vf-test-c0a02.firebaseio.com',
+  projectId: 'vf-test-c0a02',
+  storageBucket: 'vf-test-c0a02.appspot.com',
+  messagingSenderId: '809732394146'
+}
+firebase.initializeApp(config)
+firebase.auth().signInAnonymously()
+
+
+// CREATE a new task in Firebase
+function createTask (task) {
+  firebase.database().ref('tasks').push(task)
+}
+
+// READ from Firebase when woofs are added, changed, or removed
+// Add task, remove task, remove all tasks
+function getTasks () {
+
+  var ref = firebase.database().ref('tasks')
+  .on('child_added', function (snapshot) {
+    taskKey = snapshot.key
+    task = snapshot.val()
+    addTaskRow(taskKey, task)
+  })
+
+  var ref = firebase.database().ref('tasks')
+  .on('child_changed', function (snapshot) {
+    taskKey = snapshot.key
+    task = snapshot.val()
+    updateTaskRow(taskKey, task)
+  })
+
+  var ref = firebase.database().ref('tasks')
+  .on('child_removed', function (snapshot) {
+    taskKey = snapshot.key
+    deleteTaskRow(taskKey)
+  })
+}
+
+// UPDATE task in Firebase
+function updateTaskInDatabase (taskKey, complete) {
+  firebase.database().ref('tasks/'+taskKey+'/complete/').set('' + complete)
+  window.location.reload()
+}
+
+// DELETE task from Firebase
+function deleteTaskFromDatabase (taskKey) {
+  firebase.database().ref('tasks/'+taskKey+'').remove()
+  updateCount ()
+}
+
+// DELETE tasks list from Firebase
+function deleteAllTasksFromDatabase () {
+  firebase.database().ref('tasks').remove()
+  updateCount ()
+}
+
+// Load all data
+getTasks()
+
---
+++ script-ui.js
@@ -0,0 +1,102 @@
+
+var addTaskBtn = document.getElementById('add-task-js')
+var taskText = document.getElementById('task-text-js')
+var tasks = document.getElementById('todo')
+
+function addTaskRow (taskKey, task) {
+  let template = document.getElementById('task-template')
+  let clone = document.importNode(template.content, true)
+
+  clone.querySelector('.task').id = taskKey
+  clone.querySelector('.text').textContent = task.text
+  clone.querySelector('.task').classList.add(task.complete)
+  clone.querySelector('.btn-update').classList.add(task.complete)
+  clone.querySelector('.btn-update').addEventListener('click', updateTask)
+  clone.querySelector('.btn-update.danger').classList.add(task.complete)
+  clone.querySelector('.btn-update.danger').addEventListener('click', updateTask)
+  clone.querySelector('.btn-delete').addEventListener('click', deleteTask)
+
+  tasks.insertBefore(clone, tasks.firstChild)
+  taskText.value = ''
+  updateCount()
+}
+
+function createTask () {
+  let text = taskText.value || ''
+  if (!text.trim().length) return
+  createTask({
+    text: text,
+    complete: false
+  })
+}
+
+function updateTask (event) {
+  let task = this.parentElement.parentElement
+  let status = task.classList.contains('false')
+
+  updateTaskInDatabase(task.id, status)
+}
+
+// Update the status of task on page
+function updateTaskRow (taskKey) {
+  let task = document.getElementById(taskKey)
+
+  task.classList.add(task.complete)
+  task.querySelector('btn-update').add(task.complete)
+}
+
+// Remove a task from the page
+function deleteTaskRow (taskKey) {
+  let task = document.getElementById(taskKey)
+  task.parentElement.removeChild(task)
+}
+
+// Remove the clicked woof from the database
+function deleteTask () {
+  let task = this.parentElement.parentElement
+  deleteTaskFromDatabase(task.id)
+}
+
+function deleteAllTasks () {
+  deleteAllTasksFromDatabase ()
+}
+
+function updateCount () {
+  let tasks = document.getElementsByClassName('task')
+  let taskCount = document.getElementById('taskCount')
+
+  let activeTasks = []
+  for (let i = 0; i < Object.keys(tasks).length; i++) {
+    if (tasks[i].classList.contains('false'))
+      activeTasks.push(tasks[i])
+  }
+
+  taskCount.textContent = activeTasks.length
+}
+
+function toggleActiveTasks () {
+  let tasks = document.getElementsByClassName('task')
+  for (let i = 0; i < Object.keys(tasks).length; i++) {
+    if (tasks[i].classList.contains('false') && tasks[i].classList.contains('hidden')) {
+      tasks[i].classList.remove('hidden')
+    } else if (tasks[i].classList.contains('false')) {
+      tasks[i].classList.add('hidden')
+    }
+  }
+}
+
+function toggleInActiveTasks () {
+  let tasks = document.getElementsByClassName('task')
+  for (let i = 0; i < Object.keys(tasks).length; i++) {
+    if (tasks[i].classList.contains('true') && tasks[i].classList.contains('hidden')) {
+      tasks[i].classList.remove('hidden')
+    } else if (tasks[i].classList.contains('true')) {
+      tasks[i].classList.add('hidden')
+    }
+  }
+}
+
+addTaskBtn.addEventListener('click', createTask)
+document.getElementById('delete-all-tasks').addEventListener('click', deleteAllTasks)
+document.getElementById('toggleActive').addEventListener('click', toggleActiveTasks)
+document.getElementById('toggleComplete').addEventListener('click', toggleInActiveTasks)
